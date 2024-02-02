9d0816c9134355bbec3fc4616f7c4dbe89a73cca
Devysson Cardoso <devyssonsc@gmail.com>
2024-02-02 10:17:11 +0000

    Object questions created

---
+++ script.js
@@ -0,0 +1,107 @@
+const questions = [
+    {
+        id: 0,
+        question: "Quanto tempo dura uma partida de futebol?",
+        posseblyAnswers: ["75 Minutos","90 Minutos","100 Minutos", "60 Minutos"],
+        correctAnswer: 1,
+        levelDifficulty: "Easy"
+    },
+    {
+        id: 1,
+        question: "Quantos jogadores cada time de futebol tem em jogo?",
+        posseblyAnswers: ["5 jogadores","7 jogadores","11 jogadores", "10 jogadores"],
+        correctAnswer: 2,
+        levelDifficulty: "Easy"
+    },
+    {
+        id: 2,
+        question: "Quem é conhecido como 'O Rei' do futebol?",
+        posseblyAnswers: ["Pelé","Lionel Messi","Zico", "Zinedine Zidane"],
+        correctAnswer: 0,
+        levelDifficulty: "Easy"
+    },
+    {
+        id: 3,
+        question: "Qual país é o atual campeão da copa do mundo?",
+        posseblyAnswers: ["Argentina","Alemanha","Brasil", "França"],
+        correctAnswer: 0,
+        levelDifficulty: "Easy"
+    },
+    {
+        id: 4,
+        question: "Qual dos seguintes times não é campeão mundial?",
+        posseblyAnswers: ["Corinthians","Internacional","Flamengo", "Palmeiras"],
+        correctAnswer: 3,
+        levelDifficulty: "Easy"
+    },
+    {
+        id: 5,
+        question: "Qual é o país que mais tem copas do mundo?",
+        posseblyAnswers: ["Itália","Espanha","Brasil", "Alemenha"],
+        correctAnswer: 2,
+        levelDifficulty: "Medium"
+    },
+    {
+        id: 6,
+        question: "Quais times protagonizam o famoso 'El Classico'?",
+        posseblyAnswers: ["Celtics e Rangers","Real Madrid e Atlético de Madrid","Boca Júniors e River Plate", "Barcelona e Real Madrid"],
+        correctAnswer: 3,
+        levelDifficulty: "Medium"
+    },
+    {
+        id: 7,
+        question: "Quem é o maior campeão da história da Champions League?",
+        posseblyAnswers: ["Barcelona","Real Madrid","Juventus", "Milan"],
+        correctAnswer: 1,
+        levelDifficulty: "Medium"
+    },
+    {
+        id: 8,
+        question: "Qual é o jogador com maior quantidade de bolas de ouro no mundo?",
+        posseblyAnswers: ["Thierry Henry","Cristiano Ronaldo","Lionel Messi", "Neymar Jr."],
+        correctAnswer: 2,
+        levelDifficulty: "Medium"
+    },
+    {
+        id: 9,
+        question: "Qual time é o maior campeão da Premier League(Campeonato inglês)?",
+        posseblyAnswers: ["Manchester United","Tottenham","Liverpool", "Manchester City"],
+        correctAnswer: 0,
+        levelDifficulty: "Medium"
+    },
+    {
+        id: 10,
+        question: "Em qual país foi realizada a primeira copa do mundo?",
+        posseblyAnswers: ["Brasil","Inglaterra","Suécia", "Uruguai"],
+        correctAnswer: 3,
+        levelDifficulty: "Hard"
+    },
+    {
+        id: 11,
+        question: "Qual foi o maior público já registrado em uma partida de futebol?",
+        posseblyAnswers: ["195.974","199.854","194.375", "198.127"],
+        correctAnswer: 1,
+        levelDifficulty: "Hard"
+    },
+    {
+        id: 12,
+        question: "Quais times participaram da final da Champions League na temporada 2009/2010?",
+        posseblyAnswers: ["FC Internazionale x FC Bayern Munchen","Juventus FC x FC Barcelona","Real Madrid x Atlético de Madrid", "FC Liverpool x AC Milan"],
+        correctAnswer: 0,
+        levelDifficulty: "Hard"
+    },
+    {
+        id: 13,
+        question: "Qual é o maior campeão estadual do futebol brasileiro?",
+        posseblyAnswers: ["Flamengo-RJ","Paysandu-PA","Atlético-MG", "ABC-RN"],
+        correctAnswer: 3,
+        levelDifficulty: "Hard"
+    },
+    {
+        id: 14,
+        question: "Quem o Brasil enfrentou na final quando conquistou a sua primeira copa do mundo?",
+        posseblyAnswers: ["México","Suécia","Alemanha", "Hungria"],
+        correctAnswer: 1,
+        levelDifficulty: "Hard"
+    }
+];
