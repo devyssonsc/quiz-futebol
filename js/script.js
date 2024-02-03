a719d5ccfde7f69a60b4222316fa1f2e3bdba8b5
Devysson Cardoso <devyssonsc@gmail.com>
2024-02-03 18:12:43 +0000

    buttons next-question and see-result

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const $question = document.querySelector(".question");
 const $divAnswers = document.querySelector(".div-answers");
 const $levelQuestion = document.querySelector(".level-question");
+const $nextQuestion = document.querySelector("#next-question");
+const $seeResult = document.querySelector("#see-result");
 
 const questionsList = [
     {
@@ -8,80 +10,80 @@ const questionsList = [
         question: "Quanto tempo dura uma partida de futebol?",
         possibleAnswers: ["75 Minutos","90 Minutos","100 Minutos", "60 Minutos"],
         correctAnswer: "90 Minutos",
-        levelDifficulty: "Fácil (3pts)",
-        points: 3
+        levelDifficulty: "Fácil (4pts)",
+        points: 4
     },
     {
         id: 1,
         question: "Quantos jogadores cada time de futebol tem em jogo?",
         possibleAnswers: ["5 jogadores","7 jogadores","11 jogadores", "10 jogadores"],
         correctAnswer: "11 jogadores",
-        levelDifficulty: "Fácil (3pts)",
-        points: 3
+        levelDifficulty: "Fácil (4pts)",
+        points: 4
     },
     {
         id: 2,
         question: "Quem é conhecido como 'O Rei' do futebol?",
         possibleAnswers: ["Pelé","Lionel Messi","Zico", "Zinedine Zidane"],
         correctAnswer: "Pelé",
-        levelDifficulty: "Fácil (3pts)",
-        points: 3
+        levelDifficulty: "Fácil (4pts)",
+        points: 4
     },
     {
         id: 3,
         question: "Qual país é o atual campeão da copa do mundo?",
         possibleAnswers: ["Argentina","Alemanha","Brasil", "França"],
         correctAnswer: "Argentina",
-        levelDifficulty: "Fácil (3pts)",
-        points: 3
+        levelDifficulty: "Fácil (4pts)",
+        points: 4
     },
     {
         id: 4,
         question: "Qual dos seguintes times não é campeão mundial?",
         possibleAnswers: ["Corinthians","Internacional","Flamengo", "Palmeiras"],
         correctAnswer: "Palmeiras",
-        levelDifficulty: "Fácil (3pts)",
-        points: 3
+        levelDifficulty: "Fácil (4pts)",
+        points: 4
     },
     {
         id: 5,
         question: "Qual é o país que mais tem copas do mundo?",
         possibleAnswers: ["Itália","Espanha","Brasil", "Alemenha"],
         correctAnswer: "Brasil",
-        levelDifficulty: "Média (5pts)",
-        points: 5
+        levelDifficulty: "Média (6pts)",
+        points: 6
     },
     {
         id: 6,
         question: "Quais times protagonizam o famoso 'El Classico'?",
         possibleAnswers: ["Celtics e Rangers","Real Madrid e Atlético de Madrid","Boca Júniors e River Plate", "Barcelona e Real Madrid"],
         correctAnswer: "Barcelona e Real Madrid",
-        levelDifficulty: "Média (5pts)",
-        points: 5
+        levelDifficulty: "Média (6pts)",
+        points: 6
     },
     {
         id: 7,
         question: "Quem é o maior campeão da história da Champions League?",
         possibleAnswers: ["Barcelona","Real Madrid","Juventus", "Milan"],
         correctAnswer: "Real Madrid",
-        levelDifficulty: "Média (5pts)",
-        points: 5
+        levelDifficulty: "Média (6pts)",
+        points: 6
     },
     {
         id: 8,
         question: "Qual é o jogador com maior quantidade de bolas de ouro no mundo?",
         possibleAnswers: ["Thierry Henry","Cristiano Ronaldo","Lionel Messi", "Neymar Jr."],
         correctAnswer: "Lionel Messi",
-        levelDifficulty: "Média (5pts)",
-        points: 5
+        levelDifficulty: "Média (6pts)",
+        points: 6
     },
     {
         id: 9,
         question: "Qual time é o maior campeão da Premier League(Campeonato inglês)?",
         possibleAnswers: ["Manchester United","Tottenham","Liverpool", "Manchester City"],
         correctAnswer: "Manchester United",
-        levelDifficulty: "Média (5pts)",
-        points: 5
+        levelDifficulty: "Média (6pts)",
+        points: 6
     },
     {
         id: 10,
@@ -146,6 +148,13 @@ const sortQuestion = () => {
         possibleAnswersCopy.splice(sortedAnswer, 1);
     }
 
+    $nextQuestion.classList.add("hidden");
+    $seeResult.classList.add("hidden");
+
+    for (let i = 0; i < $divAnswers.children.length; i++){
+        $divAnswers.children[i].disabled = false;
+    }
+
     console.log(questionsCopy[sortedQuestion].correctAnswer);
 
     console.log(questionsCopy.length);
@@ -155,12 +164,16 @@ const sortQuestion = () => {
 $divAnswers.addEventListener("click", function(event) {
     if (event.target.tagName === "BUTTON") {
         console.log(event.target.innerText);
-        console.log(event.target.innerText.toLowerCase() === questionsCopy[sortedQuestion].correctAnswer.toLowerCase());
-        if (event.target.innerText.toLowerCase() === questionsCopy[sortedQuestion].correctAnswer.toLowerCase()) {
+        console.log(event.target.innerText === questionsCopy[sortedQuestion].correctAnswer);
+        if (event.target.innerText === questionsCopy[sortedQuestion].correctAnswer) {
             totalPoints += questionsCopy[sortedQuestion].points;
             alert("Correto");
         } else {
-            alert("Incorreto");
+            alert("Incorreto: A resposta certa é: " + questionsCopy[sortedQuestion].correctAnswer);
+        }
+
+        for (let i = 0; i < $divAnswers.children.length; i++){
+            $divAnswers.children[i].disabled = true;
         }
 
         // Remova a pergunta da lista após verificar a resposta
@@ -168,15 +181,21 @@ $divAnswers.addEventListener("click", function(event) {
 
         // Verifique se ainda há perguntas restantes
         if (questionsCopy.length > 0) {
-            // Se houver, continue com a próxima pergunta
-            sortQuestion();
+            $nextQuestion.classList.remove("hidden");
         } else {
             // Caso contrário, o quiz terminou
-            alert(`Quiz concluído! Pontuação total: ${totalPoints} pontos`);
+            $seeResult.classList.remove("hidden");
         }
     }
 });
 
+$nextQuestion.addEventListener("click", () => {
+    sortQuestion();
+})
+
+$seeResult.addEventListener("click", () => {
+    alert(`Resultado: A sua pontuação final foi: ${totalPoints}`);
+})
 
 window.addEventListener("load", () => {
     sortQuestion();
